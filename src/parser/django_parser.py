1562c1f69998ca6fe729c9fa0ad6d8ef4c7a1d0f
fargus plumdoodle <fargusplumdoodle@gmail.com>
2021-06-06 13:12:55 -0700

    Django parser gets urls

--- src/parser/django_parser.py
+++ src/parser/django_parser.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from typing import Optional
+
+from django.core.wsgi import get_wsgi_application
 
 from .parser import Parser, ParserError
 
@@ -11,11 +14,28 @@ class DjangoParserError(ParserError):
 class DjangoParser(Parser):
     name = "django"
 
-    def _run(self):
+    def __init__(self, target_dir: str, settings: Optional[dict]):
+        super().__init__(target_dir, settings)
         sys.path.insert(0, self.target_dir)
-        i = __import__(self.settings["project_app"])
-        project_settings = getattr(i, self.settings["settings_module"])
-        print(project_settings.ROOT_URLCONF)
+
+        self.project_app = __import__(self.settings["project_app"])
+        self.project_settings = getattr(
+            self.project_app, self.settings["settings_module"]
+        )
+        self._setup_django()
+
+    def _run(self):
+        root_url = __import__(self.project_settings.ROOT_URLCONF)
+        print(root_url.urls.urlpatterns)
+
+    def _setup_django(self):
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE",
+            f'{self.settings["project_app"]}.{self.settings["settings_module"]}',
+        )
+        del self.project_settings.DATABASES
+        # Forcing django to setup
+        get_wsgi_application()
 
     def validate(self):
         super().validate()
